713404806f638221f33b8a58e8e827e441a5b940
dawithagos <dawhagos@gmail.com>
2021-08-14 11:13:17 -0700

    made landing page responsive

--- main.js
+++ main.js
@@ -6,3 +6,10 @@ const changeCircleColor = (color) => {
   const circle = document.querySelector(".circle");
   circle.style.background = color;
 };
+
+const toggleMenu = () => {
+  const hamburgerToggle = document.querySelector(".hamburger");
+  const navigation = document.querySelector(".navigation");
+  hamburgerToggle.classList.toggle("active");
+  navigation.classList.toggle("active");
+};
